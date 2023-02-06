c0fa4893d3a9771c77f11ec24f892deb50631c4c
Petr Kouba <petrkouba3@gmail.com>
2023-02-06 15:09:12 +0100

    minor

--- config/paths.py
+++ config/paths.py
@@ -3,14 +3,14 @@ TOPOLOGY_PATH_TEMPLATE = "data/trajectories/{}/filtered.pdb"
 MINDIST_PATH_TEMPLATE = 'data/precomputed_trajectories/mindist-780-{}.npy'
 INFO_PATH_TEMPLATE = 'data/trajectories/{}/info.yml'
 MODEL_PATH_TEMPLATE = "data/models/{d}/model-ve-{d}-{total_states}-{{model_idx}}-intermediate-2.hdf5"
-MODEL_HISTORY_PATH_TEMPLATE = "data/model_scores/{d}/model-histories-{d}-{total_states}-{{model_idx}}.p"
+MODEL_HISTORY_PATH_TEMPLATE = "data/model_loss_scores/{d}/model-histories-{d}-{total_states}-{{model_idx}}.p"
 PRECOMPUTED_TRAJECTORIES_PATH_TEMPLATE = "data/precomputed_trajectories/mindist-780-{d}.npy"
 DATASPLITS_PATH_TEMPLATE = 'data/training_splits/{d}/model-idx-{d}-{{model_idx}}.hdf5'
 MODEL_OUTPUTS_PATH_TEMPLATE = 'data/model_outputs/{d}/data.hdf5'
 
-FRAMES_PER_JOBS_PATH_TEMPLATE = 'results/frames_for_gradient_jobs/{}_grads_{}_job_{}.npy'
-GRADIENTS_PER_JOBS_PATH_TEMPLATE = 'results/gradients/{}_grads_{}_job_{}.npy'
-CLASSIFICATION_PER_JOBS_PATH_TEMPLATE = 'results/classification/{}_classification_{}_job_{}.npy'
+FRAMES_PER_JOBS_PATH_TEMPLATE = 'results/frames_for_gradient_jobs/{d}_grads_{nf}_job_{jid}.yml'
+GRADIENTS_PER_JOBS_PATH_TEMPLATE = 'results/gradients/{d}_grads_{nf}_job_{jid}.npy'
+CLASSIFICATION_PER_JOBS_PATH_TEMPLATE = 'results/classification/{d}_classification_{nf}_job_{jid}.npy'
 CLUSTER_AVG_PATH_TEMPLATE = 'results/classification/{d}_cluster_avg_mindist_{ms}_markov_states.npy'
 
 LOCAL_SORTERS_PATH_TEMPLATE = 'results/sorters/local_sorters_{d}.yml'
